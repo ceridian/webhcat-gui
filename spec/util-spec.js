20a8202fdc66e8aa0efbea4d8f2bec16a2e460d5
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2014-12-19 11:30:31 -0500

    adding specs and tests

---
+++ spec/util-spec.js
@@ -0,0 +1,58 @@
+var u = require('../lib/util.js');
+
+Date.prototype.dateStamp = function() {  // outputs a string timestamp of a date  // yyyy-mm-dd hh:mm
+  var yyyy = this.getFullYear().toString();
+  var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
+  var dd  = this.getDate().toString();
+  var h = this.getFullHours().toString();
+  var m = this.getFullMinutes().toString();
+  return yyyy +'-'+ (mm[1]?mm:"0"+mm[0]) +'-'+ (dd[1]?dd:"0"+dd[0]) +' '+h+':'+m; // padding
+};
+Date.prototype.getFullMinutes = function () {  // if min single diget add a 0
+  if (this.getMinutes() < 10) {
+    return '0' + this.getMinutes();
+  }
+  return this.getMinutes();
+};
+Date.prototype.getFullMonth = function(){  // if month single diget add a 0
+  if (this.getMonth() < 9) {
+    return '0' + (1 + this.getMonth());
+  }
+  return 1 + this.getMonth();
+};
+Date.prototype.getFullHours = function(){  // if hour single diget add a 0
+  if (this.getHours() < 10) {
+    return '0' + this.getHours();
+  }
+  return this.getHours();
+};
+Date.prototype.getFullDate = function(){  // if date single diget add a 0
+  if (this.getDate() < 10) {
+    return '0' + this.getDate();
+  }
+  return this.getDate();
+};
+Date.prototype.getPreviousDate = function(i){
+  return new Date(new Date().setDate(new Date().getDate()-i));
+};
+
+// global variables
+debugFlag = false;
+
+describe('log()', function(){
+  it('should append to log file', function(done){
+    u.log('test', function(err){
+      expect(err).not.toBe(null);
+      done();
+    });
+  });
+});
+
+describe('error()', function(){
+  it('should append to error file', function(done){
+    u.error('test', function(err){
+      expect(err).not.toBe(null);
+      done();
+    });
+  });
+});
